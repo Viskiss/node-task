80690e9aae590e4f5530044fd91bd785d00e2ca1
Viskiss <98615844+Viskiss@users.noreply.github.com>
2022-12-02 01:59:49 +0300

    update

--- express-server/src/controllers/getTodo.js
+++ express-server/src/controllers/getTodo.js
@@ -2,29 +2,57 @@ const Todo = require("../db/models/Todo");
 
 const getTodos = {
   getAllTodos: async ({ filter }) => {
-    const todos = await Todo.find({ completed: filter === 'COMPLETED' });
+    const todos = await Todo.find({ completed: filter === "COMPLETED" });
 
-    // const activeCount = await Todo.count({ completed: false });
+    const qwe = await Todo.aggregate([
+      {
+        $facet: {
+          todos: [{ $match: {
+            completed: filter === "COMPLETED"
+          } }],
+          activeCount: [
+            {
+              $match: { completed: false },
+            },
+            {
+              $count: "activeCount",
+            }
+          ],
+          completedCount: [
+            {
+              $match: { completed: true },
+            },
+            {
+              $count: "completedCount",
+            }
+          ],
+        },
+      },
+      {
+        $project: {
+          todos: {
+            $setUnion: [
+              "$todos",
+            ]
+          },
+          activeCount: {
+            $arrayElemAt: [
+              "$activeCount.activeCount",
+              0
+            ]
+          },
+          completedCount: {
+            $arrayElemAt: [
+              "$completedCount.completedCount",
+              0
+            ]
+          }
+        }
+      }
+    ]);
 
-    // const completedCount = await Todo.count({ completed: true });
+    console.log("qwe :>> ", qwe[0]);
 
-    // console.log('activeCount :>> ', activeCount);
-
-    // console.log('completedCount :>> ', completedCount);
-
-    const qwe = await Todo.aggregate([{ "$facet": {
-      "todos": [{ "$find": { completed: filter === 'COMPLETED' } }],
-      "activeCount": [{
-        "$match": { completed: false },
-        "$count": "activeCount",
-      }],
-      "completedCount": [{
-        "$match": { completed: true },
-        "$count": "completedCount"
-      }]
-    } }]);
-
-    console.log('qwe :>> ', qwe);
     return todos;
   },
 };
