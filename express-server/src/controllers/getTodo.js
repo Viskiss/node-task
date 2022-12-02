762934761f94beac54eb64457c5e8aeaeae21622
Viskiss <zaza1771@mail.ru>
2022-12-02 10:32:45 +0300

    update

--- express-server/src/controllers/getTodo.js
+++ express-server/src/controllers/getTodo.js
@@ -3,56 +3,6 @@ const Todo = require("../db/models/Todo");
 const getTodos = {
   getAllTodos: async ({ filter }) => {
     const todos = await Todo.find({ completed: filter === "COMPLETED" });
-
-    const qwe = await Todo.aggregate([
-      {
-        $facet: {
-          todos: [{ $match: {
-            completed: filter === "COMPLETED"
-          } }],
-          activeCount: [
-            {
-              $match: { completed: false },
-            },
-            {
-              $count: "activeCount",
-            }
-          ],
-          completedCount: [
-            {
-              $match: { completed: true },
-            },
-            {
-              $count: "completedCount",
-            }
-          ],
-        },
-      },
-      {
-        $project: {
-          todos: {
-            $setUnion: [
-              "$todos",
-            ]
-          },
-          activeCount: {
-            $arrayElemAt: [
-              "$activeCount.activeCount",
-              0
-            ]
-          },
-          completedCount: {
-            $arrayElemAt: [
-              "$completedCount.completedCount",
-              0
-            ]
-          }
-        }
-      }
-    ]);
-
-    console.log("qwe :>> ", qwe[0]);
-
     return todos;
   },
 };
